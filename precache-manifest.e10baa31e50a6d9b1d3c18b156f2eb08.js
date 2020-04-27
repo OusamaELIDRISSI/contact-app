b68805a34d7cbefc18d9ba41062400d491c30e7c
Ousama <elidrissi.ousama.ma@gmail.com>
2020-04-27 18:01:10 +0000

    Updates

---
+++ precache-manifest.e10baa31e50a6d9b1d3c18b156f2eb08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fb5a36bde0424ceac82c1600b81d49",
+    "url": "/contact-app/index.html"
+  },
+  {
+    "revision": "f6c2bfda577e208e947e",
+    "url": "/contact-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6dbde56c659bd546b2bb",
+    "url": "/contact-app/static/js/2.d9b36c2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact-app/static/js/2.d9b36c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c2bfda577e208e947e",
+    "url": "/contact-app/static/js/main.36e114f0.chunk.js"
+  },
+  {
+    "revision": "40c6418f1ae9ec2eab9d",
+    "url": "/contact-app/static/js/runtime-main.05e31262.js"
+  }
+]);
