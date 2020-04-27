4a16b7f34b1c784892a96183b8efce191f82d3e4
Ousama <elidrissi.ousama.ma@gmail.com>
2020-04-27 17:06:05 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Contacts from './components/contacts';
+
+class App extends Component {
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ contacts: data })
+      })
+      .catch(console.log)
+  }
+  render() {
+    return (
+      <Contacts contacts={this.state.contacts} />
+    )
+  }
+}
+
+export default App;
